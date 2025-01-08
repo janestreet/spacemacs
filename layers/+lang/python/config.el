10d61d304a4e9ac3c8bbb4065e365ac8f9cad4d0
Aaron L. Zeng <me@bcc32.com>
2025-01-08 14:21:55 -0500

    JS: [python] Configure tab-width and fill-column through .editorconfig

--- layers/+lang/python/config.el
+++ layers/+lang/python/config.el
@@ -64,12 +64,6 @@ and `python-lsp-server' is `pylsp' (pyright and ty do not support formatting).")
 (defvar python-save-before-test t
   "If non nil, current buffer will be save before call a test function")
 
-(defvar python-fill-column 79
-  "Fill column value for python buffers")
-
-(defvar python-tab-width 4
-  "Tab width value for python buffers")
-
 (defvar python-spacemacs-indent-guess t
   "If non-nil, try to guess correct indentation settings for python buffers on opening")
 
