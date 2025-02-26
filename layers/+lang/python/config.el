50c8dc2a81e6fcb3a3761d459329710a6219b641
Aaron L. Zeng <me@bcc32.com>
2025-02-26 14:13:12 -0500

    JS: [python] Delete pyenv-mode package
    We use pyvenv instead.

--- layers/+lang/python/config.el
+++ layers/+lang/python/config.el
@@ -60,11 +60,6 @@
 (defvar python-spacemacs-indent-guess t
   "If non-nil, try to guess correct indentation settings for python buffers on opening")
 
-(defvar python-auto-set-local-pyenv-version 'on-visit
-  "Automatically set pyenv version from \".python-version\".
-
-Possible values are `on-visit', `on-project-switch' or `nil'.")
-
 (defvar python-auto-set-local-pyvenv-virtualenv 'on-visit
   "Automatically set pyvenv virtualenv from \".venv\".
 
@@ -73,9 +68,6 @@ Possible values are `on-visit', `on-project-switch' or `nil'.")
 (defvar python-sort-imports-on-save nil
   "If non-nil, automatically sort imports on save.")
 (put 'python-sort-imports-on-save 'safe-local-variable 'booleanp)
-
-(defvar spacemacs--python-pyenv-modes nil
-  "List of major modes where to add pyenv support.")
 
 (defvar spacemacs--python-pyvenv-modes nil
   "List of major modes where to add pyvenv support.")
