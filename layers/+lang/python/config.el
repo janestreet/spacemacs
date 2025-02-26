06cc1497b3337639921ee499f00738a412b4153a
Aaron L. Zeng <me@bcc32.com>
2025-02-26 14:16:36 -0500

    JS: [python] Delete pipenv package
    We use pyvenv instead.

--- layers/+lang/python/config.el
+++ layers/+lang/python/config.el
@@ -37,9 +37,6 @@
 (defvar python-lsp-git-root nil
   "If non-nil, use a development version of the language server in this folder")
 
-(defvar python-pipenv-activate nil
-  "If non-nil, activate pipenv before enabling backend")
-
 (defvar python-poetry-activate nil
   "If non-nil, activate poetry before enabling backend")
 
@@ -77,9 +74,6 @@ Possible values are `on-visit', `on-project-switch' or `nil'.")
 
 (defvar spacemacs--python-pyvenv-modes nil
   "List of major modes where to add pyvenv support.")
-
-(defvar spacemacs--python-pipenv-modes nil
-  "List of major modes where to add pipenv support.")
 
 (defvar spacemacs--python-poetry-modes nil
   "List of major modes where to add poetry support.")
