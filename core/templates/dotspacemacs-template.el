5f9d2252b86eccfbb52a8c061f170ea5ef666a37
Aaron L. Zeng <me@bcc32.com>
2025-03-24 12:51:25 -0400

    JS: Revert upstream load-hints and package-quickstart changes
    Revert "* core/templates/.spacemacs.template: more doc for the load-hints"
    
    This reverts commit a1a4b8b0338d959bedad2ed8ea8d8096e1c698d8.
    
    Revert "* doc/FAQ.org: document for load-hints and package-quickstartup"
    
    This reverts commit 469babde67c3b71ab517d56974718913b1b85c99.
    
    Revert "* core/core-configuration-layer.el: support package-quickstart"
    
    This reverts commit ec35306bdf8b36c2fc97601df1461b0721651e51.
    
    Revert "* core/core-spacemacs.el: enahnce the load-hints to fix #16652"
    
    This reverts commit 298489256ccf0f525125b6816f942ba311ec7e66.
    
    Revert "* core/core-dotspacemacs.el: load-hints default turn off"
    
    This reverts commit d52d633384fc4bc38bf8c43bd7fc0a800fa00802.
    
    Revert "* core/templates/.spacemacs.template: Add dotspacemacs-enable-load-hints"
    
    This reverts commit 0876acddb08c4f741a6ba8bb31cf5606fa660caa.
    
    Revert "Speedup the Spacemacs startup/loading with load-hints"
    
    This reverts commit bd37f169c74f541a07729d209d7b1b1adfa77a5d.

--- core/templates/dotspacemacs-template.el
+++ core/templates/dotspacemacs-template.el
@@ -338,20 +338,6 @@ It should only modify the values of Spacemacs settings."
    ;; (default t)
    dotspacemacs-maximize-window-keep-side-windows t
 
-   ;; If nil, no load-hints enabled. If t, enable the `load-hints' which will
-   ;; put the most likely path on the top of `load-path' to reduce walking
-   ;; through the whole `load-path'. It's an experimental feature to speedup
-   ;; Spacemacs on Windows. Refer the FAQ.org "load-hints" session for details.
-   dotspacemacs-enable-load-hints nil
-
-   ;; If non-nil, enable the `package-quickstart' feature to avoid activating
-   ;; all package autoloads one by one.
-   ;; Requires building and maintaining a quickstart autoload file for all
-   ;; installed packages.
-   ;; Refer to the FAQ.org "package-quickstart" section for details.
-   ;; (default nil)
-   dotspacemacs-enable-package-quickstart nil
-
    ;; If non-nil a progress bar is displayed when spacemacs is loading. This
    ;; may increase the boot time on some systems and emacs builds, set it to
    ;; nil to boost the loading time. (default t)
