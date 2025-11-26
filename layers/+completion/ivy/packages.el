eb5e09d75967cc04fbfc67520966f3f3e4d47c58
Aaron L. Zeng <me@bcc32.com>
2025-11-26 16:27:12 -0500

    JS: [ivy] Do not enable counsel-mode
    We want M-y to just use normal yank-pop.

--- layers/+completion/ivy/packages.el
+++ layers/+completion/ivy/packages.el
@@ -157,9 +157,12 @@
 
     (define-key read-expression-map (kbd "C-r") 'counsel-minibuffer-history)
     (spacemacs//counsel-search-add-extra-bindings counsel-ag-map)
-    ;; remaps built-in commands that have a counsel replacement
-    (counsel-mode 1)
-    (spacemacs|hide-lighter counsel-mode)
+
+    ;; Do not enable `counsel-mode', but add a small subset of its remappings.
+    (global-set-key [remap execute-extended-command] #'counsel-M-x) ;integrates with amx history
+    (global-set-key [remap imenu] #'counsel-imenu) ;search over whole path by default
+    (define-key minibuffer-local-map (kbd "C-r") #'counsel-minibuffer-history)
+
     ;; TODO Commands to port
     (spacemacs//ivy-command-not-implemented-yet "jI")
     ;; Set syntax highlighting for counsel search results
