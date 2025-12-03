824f8ab0de88046493581492c1d7c62614689b96
Aaron L. Zeng <me@bcc32.com>
2025-12-03 16:23:10 -0500

    [to upstream] [defaults] Remove pointless count-region alias

--- layers/+spacemacs/spacemacs-defaults/keybindings.el
+++ layers/+spacemacs/spacemacs-defaults/keybindings.el
@@ -686,7 +686,6 @@ respond to this toggle."
   "w_"  'spacemacs/maximize-horizontally
   "w|"  'spacemacs/maximize-vertically)
 ;; text -----------------------------------------------------------------------
-(defalias 'count-region 'count-words-region)
 
 (spacemacs/set-leader-keys
   "xa%" 'spacemacs/align-repeat-percent
@@ -708,7 +707,7 @@ respond to this toggle."
   "xam" 'spacemacs/align-repeat-math-oper
   "xar" 'spacemacs/align-repeat
   "xa|" 'spacemacs/align-repeat-bar
-  "xc"  'count-region
+  "xc"  'count-words-region
   "xd SPC" 'cycle-spacing
   "xdl" 'delete-blank-lines
   "xdw" 'delete-trailing-whitespace
